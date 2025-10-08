0c48146d193bf9892f0d495fdde3fc9cbf3727cc
Linh2T <linhtran2t@gmail.com>
2025-10-08 18:00:17 +0700

    remove intro tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,22 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+// import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nitroV2Plugin(),
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    // nitroV2Plugin(),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
   },
-})
+});
