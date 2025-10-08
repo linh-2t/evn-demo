c1f66c60efea4980dc6bba5ecb48125ea19291b4
Linh2T <linhtran2t@gmail.com>
2025-10-08 18:05:53 +0700

    remove devtool tanstack on production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+
 // import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -13,6 +15,9 @@ export default defineConfig({
     tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    devtools({
+      removeDevtoolsOnBuild: false,
+    }),
   ],
   resolve: {
     alias: {
